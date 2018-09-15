c39361ff45da06561820db45239e599c5efe5037
Pcheluntik <pollypuh@gmail.com>
2018-09-15 17:13:29 +0300

    big picture is open

--- js/pictures.js
+++ js/pictures.js
@@ -1,5 +1,6 @@
 'use strict';
-var i, j, k = 0;
+
+var i = 0;
 var commentArray = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -15,32 +16,31 @@ var descArray = [
   'Отдыхаем...',
   'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......',
   'Вот это тачка!'
-]
+];
 
-
-var generatePhotoArray = function() {
+var generatePhotoArray = function () {
   var photoArray = [];
 
   for (i = 0; i < 25; i++) {
-      var commentsArr = [];
-    for (j = 0; j < randomInteger(1, 5); j++) {
+    var commentsArr = [];
+    for (var j = 0; j < randomInteger(1, 5); j++) {
       commentsArr[j] = generateComments(commentArray);
     }
-      var photoNumber = i + 1;
+    var photoNumber = i + 1;
     photoArray[i] = {
       url: 'photos/' + photoNumber + '.jpg',
       likes: randomInteger(15, 200),
       comments: commentsArr,
       description: descArray[randomInteger(0, descArray.length - 1)]
-    }
+    };
   }
   return photoArray;
-}
+};
 
-function generateComments(commentArray) {
-  var comment = [];
-  for (k = 0; k < randomInteger(1, 2); k++) {
-    comment[k] = commentArray[randomInteger(0, commentArray.length - 1)];
+function generateComments(array) {
+  var comment = '';
+  for (var k = 0; k < randomInteger(1, 2); k++) {
+    comment += array[randomInteger(0, array.length - 1)] + ' ';
   }
   return comment;
 }
@@ -51,41 +51,59 @@ function randomInteger(min, max) {
   return rand;
 }
 
-
-
-
-
-/*На основе данных, созданных в предыдущем пункте и шаблона #picture создайте DOM-элементы, соответствующие фотографиям и заполните их данными из массива:
-
-Адрес изображения url подставьте как src изображения.
-Количество лайков likes подставьте как текстовое содержание элемента .picture__likes.
-Количество комментариев comments подставьте как текстовое содержание элемента .picture__stat--comments.
-*/
-
 var picTemplate = document.querySelector('#picture')
   .content
-  .querySelector('.picture');;
+  .querySelector('.picture');
 
-var renderPhotos = function(photo) {
+var renderPhotos = function (photo) {
   var picElement = picTemplate.cloneNode(true);
-  console.log(picElement.querySelector('.picture__img'));
+
   picElement.querySelector('.picture__img').setAttribute('src', photo.url);
   picElement.querySelector('.picture__comments').textContent = photo.comments.length;
   picElement.querySelector('.picture__likes').textContent = photo.likes;
 
   return picElement;
-}
+};
 
-/*Отрисуйте сгенерированные DOM-элементы в блок .pictures.
-Для вставки элементов используйте DocumentFragment.*/
-
-var drawPictures = function(photoArray) {
+var drawPictures = function (photoArray) {
   var picturesList = document.querySelector('.pictures');
   var fragment = document.createDocumentFragment();
-  for (var i = 0; i < photoArray.length; i++) {
+  for (i = 0; i < photoArray.length; i++) {
     fragment.appendChild(renderPhotos(photoArray[i]));
-  };
+  }
   picturesList.appendChild(fragment);
-}
+};
 
-drawPictures(generatePhotoArray());
+var bigPicture = document.querySelector('.big-picture');
+
+var showMainPicture = function () {
+  bigPicture.classList.remove('hidden');
+};
+
+var generateMainPicture = function (photoArray) {
+  bigPicture.querySelector('.likes-count').textContent = photoArray[0].likes;
+  bigPicture.querySelector('.big-picture__img img').setAttribute('src', photoArray[0].url);
+  bigPicture.querySelector('.comments-count').textContent = photoArray[0].comments.length;
+  var commentListArray = [];
+  for (i = 0; i < photoArray[0].comments.length; i++) {
+    commentListArray[i] = '<li class="social__comment">' +
+      '<img class="social__picture" src="img/avatar-' + randomInteger(1, 6) + '.svg"' +
+      'alt="Аватар комментатора фотографии"' +
+      'width="35" height="35">' +
+      '<p class="social__text">' + photoArray[0].comments[i] + '</p>' +
+    '</li>';
+  }
+  bigPicture.querySelector('.social__comments').innerHTML = commentListArray;
+  bigPicture.querySelector('.social__caption').textContent = photoArray[0].description;
+};
+
+var hideElement = function () {
+  document.querySelector('.social__comment-count').classList.add('visually-hidden');
+  document.querySelector('.comments-loader').classList.add('visually-hidden');
+};
+
+var photoArray = generatePhotoArray();
+drawPictures(photoArray);
+showMainPicture();
+generateMainPicture(photoArray);
+hideElement();
