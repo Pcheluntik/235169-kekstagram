4fc7ac5109c2c55952c06610544348bce7ead2fe
Pcheluntik <pollypuh@gmail.com>
2018-09-27 21:14:17 +0300

    add pic func

--- js/pictures.js
+++ js/pictures.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var ESC_KEY = 27;
 var i = 0;
 var commentArray = [
   'Всё отлично!',
@@ -18,7 +19,7 @@ var descArray = [
   'Вот это тачка!'
 ];
 
-var generatePhotoArray = function () {
+var generatePhotoArray = function() {
   var photoArray = [];
 
   for (i = 0; i < 25; i++) {
@@ -55,7 +56,7 @@ var picTemplate = document.querySelector('#picture')
   .content
   .querySelector('.picture');
 
-var renderPhotos = function (photo) {
+var renderPhotos = function(photo) {
   var picElement = picTemplate.cloneNode(true);
 
   picElement.querySelector('.picture__img').setAttribute('src', photo.url);
@@ -65,7 +66,7 @@ var renderPhotos = function (photo) {
   return picElement;
 };
 
-var drawPictures = function (photoArray) {
+var drawPictures = function(photoArray) {
   var picturesList = document.querySelector('.pictures');
   var fragment = document.createDocumentFragment();
   for (i = 0; i < photoArray.length; i++) {
@@ -76,34 +77,122 @@ var drawPictures = function (photoArray) {
 
 var bigPicture = document.querySelector('.big-picture');
 
-var showMainPicture = function () {
+var showMainPicture = function() {
   bigPicture.classList.remove('hidden');
 };
 
-var generateMainPicture = function (photoArray) {
-  bigPicture.querySelector('.likes-count').textContent = photoArray[0].likes;
-  bigPicture.querySelector('.big-picture__img img').setAttribute('src', photoArray[0].url);
-  bigPicture.querySelector('.comments-count').textContent = photoArray[0].comments.length;
+var closeMainPicture = function() {
+  bigPicture.classList.add('hidden');
+};
+var closeMainBtn = document.querySelector('.big-picture__cancel');
+closeMainBtn.addEventListener('click', closeMainPicture);
+
+
+var generateMainPicture = function(photo) {
+  bigPicture.querySelector('.likes-count').textContent = photo.querySelector('.picture__comments').textContent;
+  bigPicture.querySelector('.big-picture__img img').setAttribute('src', photo.querySelector('.picture__img').src);
+  bigPicture.querySelector('.comments-count').textContent = photo.querySelector('.picture__likes').textContent;
   var commentListArray = [];
-  for (i = 0; i < photoArray[0].comments.length; i++) {
+  var commentsArr = [];
+  for (var j = 0; j < photo.querySelector('.picture__likes').textContent; j++) {
+    commentsArr[j] = generateComments(commentArray);
+  }
+  for (i = 0; i < photo.querySelector('.picture__comments').textContent; i++) {
     commentListArray[i] = '<li class="social__comment">' +
       '<img class="social__picture" src="img/avatar-' + randomInteger(1, 6) + '.svg"' +
       'alt="Аватар комментатора фотографии"' +
       'width="35" height="35">' +
-      '<p class="social__text">' + photoArray[0].comments[i] + '</p>' +
-    '</li>';
+      '<p class="social__text">' + commentsArr[i] + '</p>' +
+      '</li>';
   }
   bigPicture.querySelector('.social__comments').innerHTML = commentListArray;
-  bigPicture.querySelector('.social__caption').textContent = photoArray[0].description;
+  bigPicture.querySelector('.social__caption').textContent = descArray[randomInteger(0, descArray.length - 1)];
+  showMainPicture();
 };
 
-var hideElement = function () {
+var hideElement = function() {
   document.querySelector('.social__comment-count').classList.add('visually-hidden');
   document.querySelector('.comments-loader').classList.add('visually-hidden');
 };
 
 var photoArray = generatePhotoArray();
 drawPictures(photoArray);
-showMainPicture();
-generateMainPicture(photoArray);
 hideElement();
+
+
+
+
+var uploadInput = document.querySelector('#upload-file');
+var uploadForm = document.querySelector('.img-upload__overlay');
+var uploadClose = document.querySelector('.img-upload__cancel');
+var uploadPin = document.querySelector('.effect-level__pin');
+var closeChangeImage = function() {
+  uploadForm.classList.add('hidden');
+  uploadClose.removeEventListener('click', closeChangeImage);
+  uploadForm.removeEventListener('keydown', closeChangeImageKey);
+}
+
+var closeChangeImageKey = function(evt) {
+  if (evt.keyCode === ESC_KEY) {
+    closeChangeImage();
+  }
+};
+
+var openChangeImage = function() {
+  uploadForm.classList.remove('hidden');
+  uploadClose.addEventListener('click', closeChangeImage);
+  document.addEventListener('keydown', closeChangeImageKey);
+  addEffects();
+}
+
+
+
+uploadInput.addEventListener('change', openChangeImage);
+
+/*На изображение может накладываться только один эффект.
+При смене эффекта, выбором одного из значений среди радиокнопок .effects__radio,
+добавить картинке внутри .img-upload__preview CSS-класс, соответствующий эффекту.
+Например, если выбран эффект .effect-chrome, изображению нужно добавить класс effects__preview--chrome.
+Интенсивность эффекта регулируется перемещением ползунка в слайдере .effect-level__pin.
+Уровень эффекта записывается в поле .scale__value. При изменении уровня интенсивности эффекта,
+CSS-стили элемента .img-upload__preview обновляются следующим образом:
+
+Для эффекта «Хром» — filter: grayscale(0..1);
+Для эффекта «Сепия» — filter: sepia(0..1);
+Для эффекта «Марвин» — filter: invert(0..100%);
+Для эффекта «Фобос» — filter: blur(0..3px);
+Для эффекта «Зной» — filter: brightness(1..3).
+При выборе эффекта «Оригинал» слайдер скрывается.
+При переключении эффектов, уровень насыщенности сбрасывается до начального значения (100%):
+слайдер, CSS-стиль изображения и значение поля должны обновляться.*/
+var addEffects = function() {
+  var effectRadioBtns = document.querySelectorAll('.effects__radio');
+  var imgUploadPreview = document.querySelector('.img-upload__preview');
+  var changeEffect = function(item) {
+    item.addEventListener('change', changeEffectCheck);
+  }
+  var changeEffectCheck = function(evt) {
+    if (evt.target.checked) {
+      var effectText = evt.target.value;
+      imgUploadPreview.classList = "";
+      imgUploadPreview.classList.add('img-upload__preview');
+      imgUploadPreview.classList.add('effects__preview--' + effectText);
+    }
+  }
+
+  for (let i = 0; i < effectRadioBtns.length; i++) {
+    changeEffect(effectRadioBtns[i])
+  };
+}
+
+
+//Показ изображения в полноэкранном режиме
+var openBigImage = function(evt) {
+  evt.preventDefault();
+  var pictureElem = evt.target.parentNode;
+  if (pictureElem.classList.contains('picture')) {
+    generateMainPicture(pictureElem);
+  }
+}
+
+document.addEventListener('click', openBigImage);
